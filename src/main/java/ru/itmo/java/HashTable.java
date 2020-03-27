8d900ab9b9441c47a4761896a08078c24dd02883
sgypsyg <62224288+sgypsyg@users.noreply.github.com>
2020-03-28 00:44:35 +0300

    request fixes

--- src/main/java/ru/itmo/java/HashTable.java
+++ src/main/java/ru/itmo/java/HashTable.java
@@ -2,35 +2,28 @@ package ru.itmo.java;
 
 public class HashTable {
 
-    private static class Entry {
-        private Object Key;
-        private Object Value;
+    private static final int DEFAULT_INITIAL_CAPACITY = 2048;
+    private static final double DEFAULT_LOADFACTOR = 0.5;
 
-        Entry(Object key, Object value) {
-            this.Key = key;
-            this.Value = value;
-        }
-    }
-
-    private Entry[] table;
+    private entry[] table;
     private boolean[] used;
     private int size = 0;
     private final double loadFactor;
     private int capacity;
     private int threshold;
 
-    private int getNextPowOfTwo(int a) {
-        int twoPow = 1;
-        while (true) {
-            if (twoPow >= a) {
-                return twoPow;
-            }
-            twoPow *= 2;
+    private static class entry {
+        private Object Key;
+        private Object Value;
+
+        entry(Object key, Object value) {
+            this.Key = key;
+            this.Value = value;
         }
     }
 
     HashTable() {
-        this(2048, 0.5);
+        this(DEFAULT_INITIAL_CAPACITY, DEFAULT_LOADFACTOR);
     }
 
     HashTable(int initialCapacity) {
@@ -40,7 +33,7 @@ public class HashTable {
     HashTable(int initialCapacity, double loadFactor) {
         this.capacity = getNextPowOfTwo(initialCapacity);
         this.loadFactor = loadFactor;
-        table = new Entry[capacity];
+        table = new entry[capacity];
         used = new boolean[capacity];
         threshold = (int) (capacity * loadFactor);
         for (int i = 0; i < capacity; i++) {
@@ -48,53 +41,13 @@ public class HashTable {
         }
     }
 
-    private int find(Object key, boolean isSearchedFree) {
-        int hash = Math.abs(key.hashCode()) % table.length;
-        if (isSearchedFree) {
-            while (true) {
-                if (table[hash] == null) {
-                    return hash;
-                }
-                hash++;
-                if (hash == table.length) {
-                    hash = 0;
-                }
-            }
-        }
-        while (true) {
-            if (table[hash] != null && table[hash].Key.equals(key)) {
-                return hash;
-            }
-            if (table[hash] == null && !used[hash]) {
-                return hash;
-            }
-            hash++;
-            if (hash == table.length) {
-                hash = 0;
-            }
-        }
-    }
-
-    void resize() {
-        Entry[] prevTable = table;
-        capacity *= 2;
-        table = new Entry[capacity];
-        used = new boolean[capacity];
-        size = 0;
-        for (Entry entry : prevTable) {
-            if (entry != null) {
-                put(entry.Key, entry.Value);
-            }
-        }
-        threshold = (int) (capacity * loadFactor);
-    }
 
     Object put(Object key, Object value) {
         if (size > threshold || size == capacity) {
             resize();
         }
 
-        Entry element = new Entry(key, value);
+        entry element = new entry(key, value);
         Object prevValue = get(key);
         boolean isNull = false;
 
@@ -119,7 +72,7 @@ public class HashTable {
 
     Object remove(Object key) {
         int index = find(key, false);
-        Entry prevElement = table[index];
+        entry prevElement = table[index];
         if (prevElement != null) {
             table[index] = null;
             size--;
@@ -130,5 +83,53 @@ public class HashTable {
 
     int size() {
         return size;
+    }
+
+    private int getNextPowOfTwo(int a) {
+        int twoPow = 1;
+        while (true) {
+            if (twoPow >= a) {
+                return twoPow;
+            }
+            twoPow *= 2;
+        }
+    }
+
+    private int find(Object key, boolean isSearchedFree) {
+        int hash = Math.abs(key.hashCode()) % table.length;
+        if (isSearchedFree) {
+            while (true) {
+                if (table[hash] == null) {
+                    return hash;
+                }
+                hash++;
+                if (hash == table.length) {
+                    hash = 0;
+                }
+            }
+        }
+        while (true) {
+            if ((table[hash] != null && table[hash].Key.equals(key)) || (table[hash] == null && !used[hash])) {
+                return hash;
+            }
+            hash++;
+            if (hash == table.length) {
+                hash = 0;
+            }
+        }
+    }
+
+    private void resize() {
+        entry[] prevTable = table;
+        capacity *= 2;
+        table = new entry[capacity];
+        used = new boolean[capacity];
+        size = 0;
+        for (HashTable.entry entry : prevTable) {
+            if (entry != null) {
+                put(entry.Key, entry.Value);
+            }
+        }
+        threshold = (int) (capacity * loadFactor);
     }
 }
